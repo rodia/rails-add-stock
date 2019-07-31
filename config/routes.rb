98d3b5e5f29957e428ce93397f05ceca8aa38536
Cesar Jaldin <rodia.piedra@gmail.com>
2019-07-31 15:14:37 -0400

    Adding user model and role mamagement

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'users/new'
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   get 'login', to: 'login#index'
